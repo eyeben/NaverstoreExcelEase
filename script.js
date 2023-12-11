de484e30dd197daa67674461fd16e3130ff2978a
eyeben <62013201+eyeben@users.noreply.github.com>
2023-12-11 22:28:25 +0900

    logic built

--- script.js
+++ script.js
@@ -30,6 +30,25 @@ function processFiles() {
         return;
     }
 
+    const sheet1 = excel1.Sheets[excel1.SheetNames[0]];
+    const sheet2 = excel2.Sheets[excel2.SheetNames[0]];
+
+    const data1 = XLSX.utils.sheet_to_json(sheet1);
+    const data2 = XLSX.utils.sheet_to_json(sheet2);
+
+    data1.forEach(row1 => {
+        const matchingRow = data2.find(row2 => row2['고객주문번호'] === row1['주문번호']);
+        if (matchingRow) {
+            row1['송장번호'] = matchingRow['송장번호'];
+        }
+    });
+
+    const updatedSheet = XLSX.utils.json_to_sheet(data1);
+    excel1.Sheets[excel1.SheetNames[0]] = updatedSheet;
+
+
 
     XLSX.writeFile(excel1, 'updated_excel1.xlsx');
+
+    document.getElementById('processButton').style.display = 'block'
 }
