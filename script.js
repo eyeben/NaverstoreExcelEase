ac23cc7df9b8c6f51b54c35e76ce4ecbe415aabe
eyeben <62013201+eyeben@users.noreply.github.com>
2023-12-12 02:46:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -35,27 +35,24 @@ function processFiles() {
     const sheet1 = excel1.Sheets[excel1.SheetNames[0]];
     const sheet2 = excel2.Sheets[excel2.SheetNames[0]];
 
-    // 첫 행을 제외하고 데이터를 읽음
     const data1 = XLSX.utils.sheet_to_json(sheet1, { defval: "", range: 1 });
-    const data2 = XLSX.utils.sheet_to_json(sheet2, { defval: ""});
+    const data2 = XLSX.utils.sheet_to_json(sheet2, { defval: "" });
 
-    data1.forEach(row1 => {
+    const updatedData = data1.map(row1 => {
         const formattedPhone = String(row1['수취인연락처1']).replace(/-/g, '').trim();
         const matchingRow = data2.find(row2 => String(row2['받는분전화번호']).trim() === formattedPhone);
-        if (matchingRow) {
-            row1['송장번호'] = matchingRow['운송장번호'];
-            console.log(`매칭 성공: ${formattedPhone}, 송장번호: ${matchingRow['운송장번호']}`);
-        } else {
-            console.log(`매칭 실패: ${formattedPhone} `);
-            console.log(data1)
-            console.log(data2)
-            row1['송장번호'] = row1['송장번호'] || "";
-        }
+
+        // 필요한 컬럼만 선택하여 새 객체 생성
+        return {
+            '상품주문번호': row1['상품주문번호'],
+            '배송방법': row1['배송방법'],
+            '택배사': row1['택배사'] || 'CJ대한통운', // '택배사'가 없는 경우 기본값 사용
+            '송장번호': matchingRow ? matchingRow['운송장번호'] : (row1['송장번호'] || "")
+        };
     });
 
-
-    // 새로운 시트 생성 시 첫 행을 포함하지 않음
-    const updatedSheet = XLSX.utils.json_to_sheet(data1);
+    // 새로운 시트 생성
+    const updatedSheet = XLSX.utils.json_to_sheet(updatedData);
 
     // 새 워크북 생성 및 시트 추가
     const newWorkbook = XLSX.utils.book_new();
@@ -64,6 +61,7 @@ function processFiles() {
     // 새 워크북 저장
     XLSX.writeFile(newWorkbook, 'updated_excel1.xlsx');
 }
+
 
 function updateProcessButtonVisibility() {
     if (excel1 && excel2) {
